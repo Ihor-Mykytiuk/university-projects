5aca0b6d71bce30a32cfa8f273c8da254dc0042e
Ihor Mykytiuk <igor14vn@gmail.com>
2025-04-06 19:05:13 +0300

    crud for books

--- app/routers/books.py
+++ app/routers/books.py
@@ -3,6 +3,7 @@ from app.database import db
 from app.repository import BookRepository
 from app.models import Book
 from typing import List
+from bson import ObjectId
 
 router = APIRouter(
     prefix="/books",
@@ -12,6 +13,23 @@ router = APIRouter(
 
 repo = BookRepository(database=db)
 
+
 @router.get("/", response_model=List[Book])
 async def get_books():
     return await repo.find_by({})
+
+
+@router.get("/{book_id}", response_model=Book)
+async def get_book(book_id: str):
+    return await repo.find_one_by_id(ObjectId(book_id))
+
+
+@router.post("/", status_code=201, response_model=Book)
+async def add_book(book: Book):
+    await repo.save(book)
+    return book
+
+@router.delete("/{book_id}", status_code=204)
+async def delete_book(book_id: str):
+    await repo.delete_by_id(ObjectId(book_id))
+    return
